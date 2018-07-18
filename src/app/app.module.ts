c051ae25b3b6439773243ef0b67b0d3ab3520936
I341372 <apurva.jain@sap.com>
2018-07-18 20:32:21 +0530

    blah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
@@ -12,7 +13,8 @@ import {ListingComponent} from './LogListingComponent/listing.component'
     ListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [LoginComponent]
--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 
 export class LoginComponent{
     title = "Logs Portal";
+    userId: string;
+    password:string;
+    login: boolean = false;
+    invalidPassowrd: boolean = false;
+    loginHandler(): void {
+        console.log('handler called'+ this.userId + this.password);
+        if(this.userId === 'admin' && this.password === 'iwonttellyou'){
+            console.log("inside true");
+            this.login = true;
+        }
+        else{
+            console.log("inside false");
+            this.login = false;
+            this.invalidPassowrd = true;
+        }
+    }
 }
