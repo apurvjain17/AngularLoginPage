4f660d759d3850d3fb1e1522a7bebe8e49089ff7
I341372 <apurva.jain@sap.com>
2018-07-20 16:50:52 +0530

    added readfile changes

--- src/app/LogListingComponent/listing.component.ts
+++ src/app/LogListingComponent/listing.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+// import * as fs from "fs";
 
 @Component({
   selector: 'pm-listing',
@@ -6,38 +9,61 @@ import { Component } from '@angular/core';
 })
 
 export class ListingComponent{
+    constructor(private http: HttpClient) {
+        // let content = fs.readFileSync('./log.csv').toString();
+        console.log('inside constructor');
+        // var myReader: FileReader = new FileReader();
+        // myReader.onloadend = function(e){
+        //     console.log(myReader.result);
+        // }
+        // myReader.readAsText("./log.csv");
+        this.http
+         .get('assets/log.csv', { responseType: 'text' })
+         .subscribe((data: any) => {
+            // data from JSON
+            let {header, ...content} = data.split('\n');
+            
+            let arr = [];
+            for(let i = Object.keys(content).length-1;i>=1;i--) {
+                arr.push( content[i].split(','));
+            }
+            this.logs = arr;
+            console.log(this.logs);
+         });
+    }
     title = "Log Listing";
-    logs = [{
-    "timestamp":"2018-07-18T14:20:34.522Z",
-    "user":"user1",
-    "log":"This is a sample log, there will be many more like this"
-},{
-    "timestamp":"2018-07-18T14:20:34.522Z",
-    "user":"user1",
-    "log":"This is a sample log, there will be many more like this"
-},{
-    "timestamp":"2018-07-18T14:20:34.522Z",
-    "user":"user1",
-    "log":"This is a sample log, there will be many more like this"
-},{
-    "timestamp":"2018-07-18T14:20:34.522Z",
-    "user":"user1",
-    "log":"This is a sample log, there will be many more like this"
-},{
-    "timestamp":"2018-07-18T14:20:34.522Z",
-    "user":"user1",
-    "log":"This is a sample log, there will be many more like this"
-},{
-    "timestamp":"2018-07-18T14:20:34.522Z",
-    "user":"user1",
-    "log":"This is a sample log, there will be many more like this"
-},{
-    "timestamp":"2018-07-18T14:20:34.522Z",
-    "user":"user1",
-    "log":"This is a sample log, there will be many more like this"
-},{
-    "timestamp":"2018-07-18T14:20:34.522Z",
-    "user":"user1",
-    "log":"This is a sample log, there will be many more like this"
-}]
+    logs:any;
+//     logs = [{
+//     "timestamp":"2018-07-18T14:20:34.522Z",
+//     "user":"user1",
+//     "log":"This is a sample log, there will be many more like this"
+// },{
+//     "timestamp":"2018-07-18T14:20:34.522Z",
+//     "user":"user1",
+//     "log":"This is a sample log, there will be many more like this"
+// },{
+//     "timestamp":"2018-07-18T14:20:34.522Z",
+//     "user":"user1",
+//     "log":"This is a sample log, there will be many more like this"
+// },{
+//     "timestamp":"2018-07-18T14:20:34.522Z",
+//     "user":"user1",
+//     "log":"This is a sample log, there will be many more like this"
+// },{
+//     "timestamp":"2018-07-18T14:20:34.522Z",
+//     "user":"user1",
+//     "log":"This is a sample log, there will be many more like this"
+// },{
+//     "timestamp":"2018-07-18T14:20:34.522Z",
+//     "user":"user1",
+//     "log":"This is a sample log, there will be many more like this"
+// },{
+//     "timestamp":"2018-07-18T14:20:34.522Z",
+//     "user":"user1",
+//     "log":"This is a sample log, there will be many more like this"
+// },{
+//     "timestamp":"2018-07-18T14:20:34.522Z",
+//     "user":"user1",
+//     "log":"This is a sample log, there will be many more like this"
+// }]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import {ListingComponent} from './LogListingComponent/listing.component'
 
@@ -14,9 +15,10 @@ import {ListingComponent} from './LogListingComponent/listing.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+     HttpClientModule
   ],
-  providers: [],
+  providers: [LoginComponent],
   bootstrap: [LoginComponent]
 })
 export class AppModule { }
